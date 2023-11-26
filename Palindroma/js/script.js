05cdcaf646e17af55be5e6b627e6c2d8a300ef56
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-27 00:45:21 +0100

    Inizialized 'Palindroma' exercise

---
+++ Palindroma/js/script.js
@@ -0,0 +1,15 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// E' vietato usare split(), reverse() e simili.
+
+const userWord = prompt('Type a word');
+
+
+
+function isWordPalindromic (word) {
+    if (word) {
+
+    } else {
+
+    }
+}
